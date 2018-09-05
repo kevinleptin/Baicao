49ac995974b4b21484ff412e6a97ee262b68e380
Penglin <kevinleptin@gmail.com>
2018-09-05 15:17:16 +0800

    Add project files.

---
+++ Baicao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Baicao.Startup))]
+namespace Baicao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
