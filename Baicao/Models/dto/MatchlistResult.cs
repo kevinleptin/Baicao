53679d35bbf057ccdacb24d110738c94fe8e21f7
void fourd <fourdvoid@gmail.com>
2018-09-06 15:14:59 +0800

    add match dto.

---
+++ Baicao/Models/dto/MatchlistResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Baicao.Models.dto
+{
+    public class MatchlistResult : ApiResult
+    {
+        public MatchlistResult()
+        {
+            List = new List<UserInfoSimple>();
+        }
+
+        public List<UserInfoSimple> List { get; set; }
+    }
+
+    public class UserInfoSimple {
+        public string Nickname
+        {
+            get;
+            set;
+        }
+
+        public string HeadImgUrl
+        {
+            get;
+            set;
+        }
+
+        public DateTime Invdate
+        {
+            get;
+            set;
+        }
+
+        public int MatchType
+        {
+            get;
+            set;
+        }
+
+        public string InvitionType
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Baicao/Models/dto/matchdto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Baicao.Models.dto
+{
+    public class matchdto
+    {
+        public matchdto()
+        {
+        }
+
+        public string openid
+        {
+            get;
+            set;
+        }
+
+        public string dadacode
+        {
+            get;
+            set;
+        }
+
+    }
+}
